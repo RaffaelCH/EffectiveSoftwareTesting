223b95b8960d46339217504df3d9f469e0efdd2a
thomi100 <joos.thomas@outlook.com>
2024-03-06 09:15:21 +0100

    added the first test and the "framework" for other tests

--- assignment01/maximum_subarray/src/test/java/zest/MaximumSubarrayTest.java
+++ assignment01/maximum_subarray/src/test/java/zest/MaximumSubarrayTest.java
@@ -1,6 +1,30 @@
 package zest;
-
+import org.junit.jupiter.api.*;
 
 class MaximumSubarrayTest {
+
+    @Nested
+    class SpecificationBasedTests {
+
+        @Test
+        @DisplayName("Test if the return is 0 when an empty array is given")
+        void testEmptyArray() {
+            int[] input = {};
+            int expected = 0;
+            int actual = MaximumSubarray.maxSubArray(input);
+            Assertions.assertEquals(expected, actual);
+        }
+
+    }
+
+    @Nested
+    class StructuralTests {
+        // TODO
+    }
+
+    @Nested
+    class MutationTests {
+        // TODO
+    }
 
 }
