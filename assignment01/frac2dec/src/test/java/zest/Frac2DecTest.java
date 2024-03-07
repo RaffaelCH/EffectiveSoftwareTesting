9921d6a9249d484f214d46623fbde2cd864abd4b
Eylul <eylul_gokce@outlook.com>
2024-03-07 19:22:01 +0100

    FracToDec Tests are written

--- assignment01/frac2dec/src/test/java/zest/Frac2DecTest.java
+++ assignment01/frac2dec/src/test/java/zest/Frac2DecTest.java
@@ -1,5 +1,57 @@
 package zest;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-class Frac2DecTest {
- }
+public class Frac2DecTest {
+
+    @Test
+    public void testSimpleFraction() {
+        assertEquals("0.25", Frac2Dec.fractionToDecimal(1, 4));
+    }
+
+    @Test
+    public void testRepeatingFraction() {
+        assertEquals("0.(6)", Frac2Dec.fractionToDecimal(2, 3));
+    }
+
+    @Test
+    public void testNoFraction() {
+        assertEquals("3", Frac2Dec.fractionToDecimal(3, 1));
+    }
+
+    @Test
+    public void testNegativeFraction() {
+        assertEquals("-0.25", Frac2Dec.fractionToDecimal(-1, 4));
+    }
+
+    @Test
+    public void testBothNegative() {
+        assertEquals("0.25", Frac2Dec.fractionToDecimal(-1, -4));
+    }
+
+    @Test
+    public void testZeroNumerator() {
+        assertEquals("0", Frac2Dec.fractionToDecimal(0, 3));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testZeroDenominator() {
+        Frac2Dec.fractionToDecimal(1, 0);
+    }
+
+    @Test
+    public void testLargeNumbers() {
+        assertEquals("-1073741824", Frac2Dec.fractionToDecimal(-2147483648, 2));
+    }
+
+    @Test
+    public void testLongRepeating() {
+        assertEquals("0.(142857)", Frac2Dec.fractionToDecimal(1, 7));
+    }
+
+    @Test
+    public void testBothOne() {
+        assertEquals("1", Frac2Dec.fractionToDecimal(1, 1));
+    }
+}
