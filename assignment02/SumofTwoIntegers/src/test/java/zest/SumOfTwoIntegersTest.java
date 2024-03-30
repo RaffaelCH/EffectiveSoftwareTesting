cf2eb81b579c53050dbed285a761221f80ab4b19
rafb <raffael.botschen@zuehlke.com>
2024-03-30 09:23:00 +0100

    Add example tests.

--- assignment02/SumofTwoIntegers/src/test/java/zest/SumOfTwoIntegersTest.java
+++ assignment02/SumofTwoIntegers/src/test/java/zest/SumOfTwoIntegersTest.java
@@ -12,16 +12,21 @@ class SumOfTwoIntegersTest {
     public void positiveAdditionTest() {
         assertEquals(3, classUnderTest.getSum(1, 2), "smaller + larger");
         assertEquals(3, classUnderTest.getSum(2, 1), "larger + smaller");
+        assertEquals(42, classUnderTest.getSum(42, 0), "one of the arguments is 0");
         assertEquals(63, classUnderTest.getSum(42, 21), "same number of bits == 0");
         assertEquals( Integer.MAX_VALUE, classUnderTest.getSum(Integer.MAX_VALUE - 1, 1), "result is largest 32bit int");
+        // assertEquals( Integer.MAX_VALUE + 1, classUnderTest.getSum(Integer.MAX_VALUE, 1), "overflow behaves like + operator");
+        // assertEquals( (long) Integer.MAX_VALUE + 1, classUnderTest.getLongSum(Integer.MAX_VALUE, 1), "overflow");
     }
 
     @Test
     public void negativeAdditionTest() {
         assertEquals(-3, classUnderTest.getSum(-1, -2), "larger + smaller");
         assertEquals(-3, classUnderTest.getSum(-2, -1), "smaller + larger");
+        assertEquals(-42, classUnderTest.getSum(-42, 0), "one of the arguments is 0");
         assertEquals(-63, classUnderTest.getSum(-52, -11), "same number of bits == 0");
         assertEquals( Integer.MIN_VALUE, classUnderTest.getSum(Integer.MIN_VALUE + 1, -1), "result is smallest 32bit int");
+        // assertEquals( Integer.MIN_VALUE - 1, classUnderTest.getSum(Integer.MIN_VALUE, -1), "underflow behaves like + operator");
     }
 
     @Test
