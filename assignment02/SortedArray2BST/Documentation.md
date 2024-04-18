dbacbe4204d863be62da8e755ddfc2cf8b697388
thomi100 <joos.thomas@outlook.com>
2024-04-18 21:23:27 +0200

    Task 1 of SortedArrayToBST

---
+++ assignment02/SortedArray2BST/Documentation.md
@@ -0,0 +1,11 @@
+# SortedArray2BST
+## Task 1: Code Coverage
+We applied structural testing to apply 100% line coverage.
+We reached not only 100% line coverage but also 100% branch coverage.
+We used the JaCoCo plugin to analyze and generate coverage reports, the screenshots can be found in the ['Assets' folder](./Assets) as described in the assignment's instructions.
+
+## Task 2: Designing Contracts
+
+## Task 3: Testing Contracts
+
+## Task 4: Property-Based Testing
--- assignment02/SortedArray2BST/src/test/java/zest/SortedArrayToBSTTest.java
+++ assignment02/SortedArray2BST/src/test/java/zest/SortedArrayToBSTTest.java
@@ -1,6 +1,53 @@
 package zest;
 
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 class SortedArrayToBSTTest {
 
- 
+    @Nested
+    class Task1CodeCoverage {
+
+        /*
+        - Test empty array (size 0)
+        - Test array with one element
+        - Test array with subsequent elements
+        - Test array which is already in BST form
+         */
+
+        @Test
+        void testEmptyArray() {
+            SortedArrayToBST sortedArrayToBST = new SortedArrayToBST();
+            int[] nums = {};
+            TreeNode node = sortedArrayToBST.sortedArrayToBST(nums);
+            assertNull(node);
+        }
+
+        @Test
+        void testArrayWithOneElement() {
+            SortedArrayToBST sortedArrayToBST = new SortedArrayToBST();
+            int[] nums = {1};
+            TreeNode node = sortedArrayToBST.sortedArrayToBST(nums);
+            assert(node.val == 1);
+            assertNull(node.left);
+            assertNull(node.right);
+        }
+
+        @Test
+        void testArrayWithSubsequentElements() {
+            SortedArrayToBST sortedArrayToBST = new SortedArrayToBST();
+            int[] nums = {1, 2, 3, 4, 5};
+            TreeNode node = sortedArrayToBST.sortedArrayToBST(nums);
+            assert(node.val == 3);
+            assert(node.left.val == 1);
+            assert(node.right.val == 4);
+            assert(node.left.right.val == 2);
+            assert(node.right.right.val == 5);
+        }
+
+
+    }
+
 }
