ab692d814fb614f4de7689e6877f44474ed2c9ef
nikita-uzh <nikitaammini.amitabh@uzh.ch>
2024-04-20 18:56:53 +0200

    completed tasks for climbing stairs problem

--- assignment02/ClimbingStairs/src/main/java/zest/ClimbingStairs.java
+++ assignment02/ClimbingStairs/src/main/java/zest/ClimbingStairs.java
@@ -1,19 +1,32 @@
 package zest;
 
 public class ClimbingStairs {
+
+    /**
+     * @param value number of steps to reach the top. has to be positive integer.
+     * @return number of distinct ways to climb to the top. has to be non-negative integer
+     */
     public long climbStairs(int n) {
+        // assumption: positive (in description of constraints) means not zero
+        assert n>0;
+
         if (n <= 2) {
             return n;
         }
         long oneStepBefore = 2;
         long twoStepsBefore = 1;
-        long allWays = 0;
+        long allWays = 0; // allWays is still trivially >=0 by definition here so no assert
 
         for (int i = 2; i < n; i++) {
             allWays = oneStepBefore + twoStepsBefore;
+            assert allWays >= 0;
             twoStepsBefore = oneStepBefore;
             oneStepBefore = allWays;
+            assert twoStepsBefore >= 0;
+            assert oneStepBefore >= 0;
         }
+
+        assert allWays >=0;
         return allWays;
     }
 }
--- assignment02/ClimbingStairs/src/test/java/zest/ClimbingStairsTest.java
+++ assignment02/ClimbingStairs/src/test/java/zest/ClimbingStairsTest.java
@@ -1,8 +1,53 @@
 package zest;
 
+import net.jqwik.api.*;
+import net.jqwik.api.constraints.IntRange;
+import org.junit.jupiter.api.Test;
 
-class ClimbingStairsTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+public class ClimbingStairsTest {
+    private final ClimbingStairs cs = new ClimbingStairs();
+
+    @Test
+    void normalInput(){
+        assertEquals(1, cs.climbStairs(1));
+        assertDoesNotThrow(() -> cs.climbStairs(1));
+
+        assertEquals(2, cs.climbStairs(2));
+        assertDoesNotThrow(() -> cs.climbStairs(2));
+
+        assertEquals(3, cs.climbStairs(3));
+        assertDoesNotThrow(() -> cs.climbStairs(3));
+
+        assertEquals(5, cs.climbStairs(4));
+        assertDoesNotThrow(() -> cs.climbStairs(4));
+
+        assertEquals(1836311903, cs.climbStairs(45));
+        assertDoesNotThrow(() -> cs.climbStairs(45));
+
+        // those outputs are all >= 0 so post condition holds
+        // no assertion error is thrown so allWays stayed >= during loop execution (and so did helper values oneStepBefore, twoStepsBefore)
+    }
+
+    @Test
+    void nonPositiveInput(){
+        // pre-condition being violated is caught through assertion error
+        assertThrows(AssertionError.class, () -> cs.climbStairs(-2));
+        assertThrows(AssertionError.class, () -> cs.climbStairs(0));
+        assertThrows(AssertionError.class, () -> cs.climbStairs(Integer.MIN_VALUE));
+    }
+
+    @Property
+    void trivialInput(@ForAll @IntRange(min = 1, max = 2) int n) {
+        assert(cs.climbStairs(n) == n);
+    }
+
+    // 90 is last number whose result does not lead to jqwik shrinking issues
+    @Property
+    void isPositive(@ForAll @IntRange(min = 2, max = 91) int n) {
+        assert(cs.climbStairs(n) > 0);
+    }
 }
 
 
